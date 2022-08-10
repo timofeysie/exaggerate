6d26136481a2c72722269adee13e30a2d2714208
tim_curchod <timofeyc@hotmail.com>
2022-08-10 21:11:42 +1000

    added e2e test for elaborate

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,11 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/elaborate')
+      .expect(200)
+      .expect('This action returns all elaborate');
+  });
 });
